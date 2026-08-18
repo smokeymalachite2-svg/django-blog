c4ef873735815e6d7bfa4fae6a86d9e1793acbef
zayn <smokeymalachite2@gmail.com>
2026-08-19 03:31:05 +0500

    Initial Django project

---
+++ Blog/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+
+from .models import Post, Author, Tag
+
+# Register your models here
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
